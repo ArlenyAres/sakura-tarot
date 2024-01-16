7e99f248e1bfcc9f7d6ec18fbc84d732600a6971
lolamindi <laura_mday@yahoo.co.uk>
2024-01-16 13:03:24 +0100

    add home page designa and features

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,30 @@
 import Head from 'next/head';
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Overlock } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const overlockFont = Overlock({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export const metadata = {
-  title: "MYSTIKA Sakura Tarot",
-  description: "Lecturas virtuales de tu pasado, presente y futuro",
+  title: 'MYSTIKA Sakura Tarot',
+  description: 'Lecturas virtuales de tu pasado, presente y futuro',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <Head>
+       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* <Header /> */}
+        <main>{children}</main>
+        {/* <Footer /> */}
+      </body>
     </html>
   );
 }
+
+
