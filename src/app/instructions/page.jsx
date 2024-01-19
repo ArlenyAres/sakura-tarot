19a21100f9035e20aa3f301f4a7f1424c8fcdb94
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-19 11:48:06 +0100

    instructions page created

---
+++ src/app/instructions/page.jsx
@@ -0,0 +1,11 @@
+import Carousel from '../components/Carousel/Carousel';
+
+export default function Instructions() {
+  return (
+    <>
+      <main>
+        <Carousel/>
+      </main>
+    </>
+  )
+}
