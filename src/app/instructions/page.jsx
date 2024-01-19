bdd5dbbc7d5058d131d4da312b0fbb0ba5a417c8
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-19 12:02:58 +0100

    folder name changed

---
+++ src/app/instructions/page.jsx
@@ -0,0 +1,12 @@
+import Carousel from '../components/Carousel/Carousel';
+
+export default function Instructions() {
+  return (
+    <>
+      <main>
+        <Carousel/>
+        <p></p>
+      </main>
+    </>
+  )
+}
