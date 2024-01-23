501f3f4d5c7dbf4601f08e275dfcd703e0dca111
lolamindi <laura_mday@yahoo.co.uk>
2024-01-23 13:44:36 +0100

    add style to modal

--- src/app/modal/page.jsx
+++ src/app/modal/page.jsx
@@ -6,39 +6,32 @@ import React from 'react';
 
 function Modal() {
   return (
-    <>
-      <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-          <h3 className="text-xl font-semibold text-purple-text dark:text-white">
-            Entra tu nombre para guardar las lecturas
-          </h3>
-          <span className="sr-only">Close modal</span>
-        </div>
-        <div className="p-4 md:p-5">
-          <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Nombre
-          </label>
-          <input
-            type="name"
-            name="firstName"
-            id="firstName"
-            placeholder="Nombre"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <div className="flex justify-between">
-          <div className="flex items-start">
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="w-full text-white bg-purple-dark hover:bg-purple-medium focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Guardar
-        </button>
+    <div className="w-full md:w-2/3 lg:w-1/2 xl:w-2/5 2xl:w-1/3 flex flex-col items-center pb-5 bg-beige rounded-lg shadow dark:bg-purple-text">
+      <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-purple-text">
+        <h3 className="text-base font-semibold text-purple-text dark:text-white text-center">
+          Entra tu nombre para guardar las lecturas de tarot:
+        </h3>
+        <span className="sr-only">Close modal</span>
       </div>
-    </>
+      <div className="p-4 md:p-5">
+        <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-purple-text dark:text-white">
+        </label>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          placeholder="Nombre"
+          className="w-full bg-white border border-lilac-medium text-purple-text text-sm rounded-lg focus:ring-purple-dark focus:border-purple-medium p-2.5 dark:bg-purple-dark dark:border-purple-dark dark:placeholder-lilac-medium dark:text-white"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="md:w-[28%] lg:w-1/6 xl:w-[28%] 2xl:w-2/6 text-white bg-purple-dark hover:bg-purple-medium focus:outline-none focus:ring-purple-medium font-bold rounded-full text-lg px-5 py-2.5 text-center dark:bg-purple-medium dark:hover:bg-purple-dark dark:focus:ring-purple-dark"
+      >
+        Guardar
+      </button>
+    </div>
   );
 }
 
