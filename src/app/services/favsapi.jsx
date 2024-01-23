9621f28fd634212af2d2b222241e651917e6cc6f
Carolina Chagas <thamelo.win@gmail.com>
2024-01-23 15:13:34 +0100

    add favs api

--- src/app/services/favsapi.jsx
+++ src/app/services/favsapi.jsx
@@ -1,5 +1,17 @@
 import axios from "axios"
 
 export const favsapi = () => {
-    
-}
+
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.headers.post['Accept'] = 'application/json';
+
+    const baseUrl = 'http://localhost:8000/users'
+
+    const createFav = async (data) => {
+        const response = await axios.post(baseUrl, data);
+        return response;
+    }
+    return {
+        createFav
+    }
+    }
