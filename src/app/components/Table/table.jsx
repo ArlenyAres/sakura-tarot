b5940ead6555a8fa17c060832e68b5f53efce94c
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-25 13:08:35 +0100

    add array map to table

--- src/app/components/Table/table.jsx
+++ src/app/components/Table/table.jsx
@@ -1,22 +1,26 @@
-'use client'
-import { useEffect, useState } from 'react';
 import { getAll } from '@/app/services/historialUser';
 
 
-const Table = () => {
-    const [dataTable, setDataTable] = useState([]);
-    const info= getAll();
+const Table = async () => {
+    const info= await getAll();
     console.log(info);
     
-
-    // useEffect(()=>{
-    //     historial.getAll().then(res => console.log(res.data.data) )
-    //     console.log(res.data)
-    // })
 
     return (
         <div>
             <h1 className="text-center">Historial de Usuarios</h1>
+            <ul>
+                {
+                    info.map((item) => 
+                        (<li key={item.id} > { Array.isArray(item.reading)
+                            ? 
+                            (item.reading.map((card) => (<div key={card.id}>{card.meaning}</div>))
+                        ): (<p>no results</p>)
+                        }
+                            </li>)
+                    )
+                }
+            </ul>
         </div>
     )
 }
